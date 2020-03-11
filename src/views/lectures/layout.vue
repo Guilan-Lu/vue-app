c86a0abf07b8b276c6b789c62524e141c09a226e
email <redmanfist@outlook.com>
2020-03-11 17:53:11 +0800

    reponsive layout is completed

---
+++ src/views/lectures/layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid grid-list-md>
+    <v-row row-wrap>
+      <v-col xs12 sm6 md4 lg3 xl2 v-for="i in 9" :key="i">
+        <v-card color="red">
+          <v-card-title primary-title v-if="!$vuetify.breakpoint.xs">
+            {{$vuetify.breakpoint}}
+          </v-card-title>
+          <v-card-text>
+            I have rich experience with Vue js
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
